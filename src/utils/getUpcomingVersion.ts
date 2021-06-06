d88dc1cbaf40aefbfef57c112e9e8508f44b745f
JenWa <37512588+JenWa@users.noreply.github.com>
2021-06-06 20:01:56 +0200

    feat: Add option to display commits in Changelog which don't belong to a release version yet

--- src/utils/getUpcomingVersion.ts
+++ src/utils/getUpcomingVersion.ts
@@ -6,13 +6,18 @@ const isUpcomingReleaseDefined = (tags: string[], packageVersion: string) =>
   packageVersion === "0.0.0" ||
   (packageVersion.includes("-") && !OPTIONS.releasesOnly);
 
-export const getUpcomingVersion = (tags: string[]): string => {
+export const getUpcomingVersion = (tags: string[]): UpcomingRelease => {
   const packageJson = fs.readFileSync("package.json").toString();
   const version = JSON.parse(packageJson).version || "0";
   console.log(OPTIONS.tagPrefix + version);
   if (version === "0")
     console.warn("Can not read version from your package.json");
   return isUpcomingReleaseDefined(tags, version)
-    ? "Upcoming release (not yet planned)"
-    : version;
+    ? { isReady: false, version: "Upcoming release (not yet planned)" }
+    : { isReady: true, version };
+};
+
+export type UpcomingRelease = {
+  isReady: boolean;
+  version: string;
 };
--- src/utils/passCLIArgumentsToOptions.ts
+++ src/utils/passCLIArgumentsToOptions.ts
@@ -9,11 +9,15 @@ export const passCLIArgumentsToOptions = (): void => {
       "-t": "--tag-prefix",
       "--releases-only": Boolean,
       "-r": "--releases-only",
+      "--has-upcoming-section": Boolean,
+      "-u": "--has-upcoming-section",
     },
     { permissive: true, argv: process.argv.slice(2) }
   );
   OPTIONS.tagPrefix = args["--tag-prefix"] ?? OPTIONS.tagPrefix;
   OPTIONS.releasesOnly = args["--releases-only"] ?? OPTIONS.releasesOnly;
+  OPTIONS.hasUpcomingSection =
+    args["--has-upcoming-section"] ?? OPTIONS.hasUpcomingSection;
   if (args._.length > 0) {
     console.warn(
       `Following arguments aren't recognized. Ignoring them: \n${args._}`
