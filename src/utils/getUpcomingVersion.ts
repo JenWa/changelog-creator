9c54ec29f0ca84c49366cc7c8769b729649f26fe
JenWa <37512588+JenWa@users.noreply.github.com>
2021-06-30 21:14:54 +0200

    fix(UpcomingSection): Move commits related to an undefined version to Upcoming section

--- src/utils/getUpcomingVersion.ts
+++ src/utils/getUpcomingVersion.ts
@@ -1,22 +1,25 @@
-import fs from "fs";
+import { readFileSync } from "fs";
 import OPTIONS from "../options";
 
-const isUpcomingReleaseDefined = (tags: string[], packageVersion: string) =>
+export interface UpcomingRelease {
+  isReady: boolean;
+  version: string;
+}
+
+const isVersionAlreadyAssigned = (
+  tags: string[],
+  packageVersion: string | undefined
+) =>
+  !packageVersion ||
   tags.includes(`${OPTIONS.tagPrefix}${packageVersion}`) ||
   packageVersion === "0.0.0" ||
   (packageVersion.includes("-") && !OPTIONS.releasesOnly);
 
 export function getUpcomingVersion(tags: string[]): UpcomingRelease {
-  const packageJson = fs.readFileSync("package.json").toString();
-  const version = JSON.parse(packageJson).version || "0";
-  if (version === "0")
-    console.warn("Can not read version from your package.json");
-  return isUpcomingReleaseDefined(tags, version)
+  const packageJson = readFileSync("package.json").toString();
+  const version = JSON.parse(packageJson).version;
+  if (!version) console.warn("Can not read version from your package.json");
+  return isVersionAlreadyAssigned(tags, version)
     ? { isReady: false, version: "Upcoming release (not yet planned)" }
     : { isReady: true, version };
 }
-
-export type UpcomingRelease = {
-  isReady: boolean;
-  version: string;
-};
