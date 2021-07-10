1f10752abe45107288b3b5ab605717e7b2cc29d4
JenWa <37512588+JenWa@users.noreply.github.com>
2021-07-11 00:29:38 +0200

    fix(tagPrefix): Pass to semver and add prefix to package version

--- src/utils/getUpcomingVersion.ts
+++ src/utils/getUpcomingVersion.ts
@@ -11,13 +11,13 @@ const isVersionAlreadyAssigned = (
   packageVersion: string | undefined
 ) =>
   !packageVersion ||
-  tags.includes(`${OPTIONS.tagPrefix}${packageVersion}`) ||
-  packageVersion === "0.0.0" ||
+  tags.includes(packageVersion) ||
+  packageVersion === `${OPTIONS.tagPrefix}0.0.0` ||
   (packageVersion.includes("-") && !OPTIONS.releasesOnly);
 
 export function getUpcomingVersion(tags: string[]): UpcomingRelease {
   const packageJson = readFileSync("package.json").toString();
-  const version = JSON.parse(packageJson).version;
+  const version = `${OPTIONS.tagPrefix}${JSON.parse(packageJson).version}`;
   if (!version) console.warn("Can not read version from your package.json");
   return isVersionAlreadyAssigned(tags, version)
     ? { isReady: false, version: "Upcoming release (not yet planned)" }
