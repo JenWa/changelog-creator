c262c60152296bbf3b4751c2067dc610866909c8
JenWa <37512588+JenWa@users.noreply.github.com>
2021-07-11 17:39:09 +0200

    feat: Add option to define file name of changelog

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 import { createChangelog } from "./createChangelog";
+import OPTIONS from "./options";
 
 export const cli = (): Promise<void> =>
   createChangelog()
-    .then(() => console.info("Created Changelog.md"))
+    .then(() => console.info(`Created ${OPTIONS.fileName}`))
     .catch((error) => console.error(error));
