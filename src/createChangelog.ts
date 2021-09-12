d2108ee325273a003c0d2fd4cdb951e480eca396
JenWa <37512588+JenWa@users.noreply.github.com>
2021-09-12 13:56:51 +0200

    feat(date): Write release date next to version in Changelog

--- src/createChangelog.ts
+++ src/createChangelog.ts
@@ -2,12 +2,19 @@ import { createWriteStream } from "fs";
 import OPTIONS from "./options";
 import { getRepoTags, getVersionChanges, VersionChanges } from "./utils";
 
+function getVersionHeadline(version: string, date?: string) {
+  const versionDateText = date ? `(${date})` : "";
+  return `## ${version} ${versionDateText} \n`;
+}
+
 export async function createChangelog(): Promise<void> {
   const writeable = createWriteStream(OPTIONS.fileName, { flags: "w" });
   const sortedRepoTags = (await getRepoTags()).reverse();
   const versions = (await getVersionChanges(sortedRepoTags)).reverse();
   versions.forEach((versionChanges: VersionChanges) => {
-    writeable.write(`## ${versionChanges.version} \n`);
+    writeable.write(
+      getVersionHeadline(versionChanges.version, versionChanges.date)
+    );
     Object.entries(versionChanges.commits).forEach(([type, commits]) => {
       if (OPTIONS.sortBy && commits.length > 0) {
         writeable.write(`### ${type} \n`);
--- src/utils/getVersionChanges.ts
+++ src/utils/getVersionChanges.ts
@@ -1,10 +1,12 @@
 import OPTIONS from "../options";
 import { getCommits, GroupedCommits } from "./getCommits";
+import { getTagDatePairs } from "./getTagDatePairs";
 import { getUpcomingVersion } from "./getUpcomingVersion";
 
 export interface VersionChanges {
   version: string;
   commits: GroupedCommits;
+  date?: string;
 }
 
 export async function getVersionChanges(
@@ -14,12 +16,14 @@ export async function getVersionChanges(
   const addUpcomingSection =
     OPTIONS.hasUpcomingSection || upcomingRelease.isReady;
   const groupedCommits: VersionChanges[] = [];
+  const tagDatePairs = getTagDatePairs();
 
   let previousTag: string | undefined = undefined;
   for (const tag of tags) {
     groupedCommits.push({
       version: tag,
       commits: await getCommits({ from: previousTag, to: tag }),
+      date: tagDatePairs[tag],
     });
     previousTag = tag;
   }
@@ -27,6 +31,7 @@ export async function getVersionChanges(
   if (addUpcomingSection) {
     groupedCommits.push({
       version: upcomingRelease.version,
+      date: new Date().toISOString().slice(0, 10),
       commits: await getCommits({ from: tags[tags.length - 1] }),
     });
   }
